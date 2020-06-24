075c6c50c6921f56e2df2f6a93f17df865414625
Thomas Stussi <tstussi06@gmail.com>
2020-06-24 15:52:31 -0700

    added nametag

---
+++ app.js
@@ -0,0 +1,11 @@
+const myButton = document.getElementById('my-special-button');
+
+myButton.addEventListener('click', () => {
+    // post-click logic goes here!
+})
+
+const myInput = document.getElementById('my-special-input');
+
+myInput.addEventListener('input', () => {
+    // post-click logic goes here!
+})
